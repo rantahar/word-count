d3e20a531260bc9d78914bdd65c054c63d243613
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2024-02-28 14:15:15 +0200

    Add runner script

---
+++ run.sh
@@ -0,0 +1,11 @@
+python source/wordcount.py data/isles.txt processed_data/isles.data
+python source/plotcount.py processed_data/isles.data results/isles.png
+
+python source/wordcount.py data/abyss.txt processed_data/abyss.data
+python source/plotcount.py processed_data/abyss.data results/abyss.png
+
+python source/wordcount.py data/last.txt processed_data/last.data
+python source/plotcount.py processed_data/last.data results/last.png
+
+python source/wordcount.py data/sierra.txt processed_data/sierra.data
+python source/plotcount.py processed_data/sierra.data results/sierra.png
